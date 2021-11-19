ce8ff7bd4f734ada344120d8159d8433af953e46
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-19 07:45:50 +0100

    fetch data and pass as props to slider template component

--- src/widgets/NewsSlider/slider.js
+++ src/widgets/NewsSlider/slider.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { Component } from "react/cjs/react.production.min";
+import React, { Component } from "react";
+import axios from "axios";
+
+import SliderTemplate from "./sliderTemplate";
 
 class NewsSlider extends Component {
+
+    state = {
+        news: []
+    }
+
+    componentWillMount () {
+        axios.get('http://localhost:3003/articles?_start=0&_end=3')
+        .then ( (response) => {
+            this.setState({
+                news: response.data
+            })
+        })
+    }
+
     render (){
+        // console.log(this.state.news)
         return (
-            <div>
-                Slider Components
-            </div>
+            <SliderTemplate data={this.state.news} />
         )
     }
 }
---
+++ src/widgets/NewsSlider/sliderTemplate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SliderTemplate = (props) => {
+    // console.log(props)
+
+    return (
+        <div>
+            From Slider Template
+        </div>
+    )
+}
+
+export default SliderTemplate;
