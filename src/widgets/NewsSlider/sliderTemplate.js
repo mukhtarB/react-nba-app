45befe40a0fc12422e31c0b883ed35d8f3a32d93
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-19 12:26:19 +0100

    carousel

--- src/widgets/NewsSlider/sliderTemplate.js
+++ src/widgets/NewsSlider/sliderTemplate.js
@@ -1,12 +1,26 @@
 import React from "react";
 
+import Slider from "react-slick";
+
 const SliderTemplate = (props) => {
     // console.log(props)
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 1,
+        SlidesToScroll:1
+    }
+
     return (
-        <div>
-            From Slider Template
-        </div>
+        <Slider {...settings}>
+            <div> Template 1 </div>
+            <div> Template 2 </div>
+            <div> Template 3 </div>
+            <div> Template 4 </div>
+        </Slider>
     )
 }
 
