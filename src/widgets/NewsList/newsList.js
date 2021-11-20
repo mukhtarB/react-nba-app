c2b8e5d80555aaccbb124e692269b92c99467d11
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-20 08:06:18 +0100

    feat: newslist Template

--- src/widgets/NewsList/newsList.js
+++ src/widgets/NewsList/newsList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import style from './newsList.module.css';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { url } from '../../config'
@@ -24,10 +25,36 @@ class NewsList extends Component {
         })
     }
 
+    renderNews = (type) => {
+        let template = null;
+
+        switch (type) {
+            case ('card'):
+                template = this.state.items.map( (item, i) => {
+                    return (
+                        <div>
+                            <div className={style.newsList_item}>
+                                <Link to = {`/articles/${item.id}`}>
+                                    <h2>{item.title}</h2>
+                                </Link>
+                            </div>
+                        </div>
+                    )
+                })
+                break;
+        
+            default:
+                template = null;
+                break;
+        }
+
+        return template;
+    }
+
     render () {
         return (
             <div>
-                News List Component
+                { this.renderNews(this.props.type) }
             </div>
         )
     }
