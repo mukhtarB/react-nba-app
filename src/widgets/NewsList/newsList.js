ba19388a09b6dbb19b28284bc8f5bd0e539a25f2
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-20 07:33:49 +0100

    added newsList component & react transitions package

---
+++ src/widgets/NewsList/newsList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+
+class NewsList extends Component {
+    render () {
+        return (
+            <div>
+                News List Component
+            </div>
+        )
+    }
+}
+
+export default NewsList;
