7b0944df24a4784f626d4f7ff386430d8e2de471
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-25 12:31:50 +0100

    routing & compnent frame

---
+++ src/components/Articles/Videos/Video/video_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class VideoArticle extends Component {
+    render () {
+        return (
+            <div>
+                Video Article Component
+            </div>
+        )
+    }
+}
+
+export default VideoArticle;
