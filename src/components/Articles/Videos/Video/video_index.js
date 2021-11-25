d8a2c4eccc3cd55952684dc037558183d7b92b16
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-25 14:46:52 +0100

    axios request

--- src/components/Articles/Videos/Video/video_index.js
+++ src/components/Articles/Videos/Video/video_index.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { url } from '../../../../config';
+import withRouterHOC from "../../../../hoc/withRouter/withRouter";
+
+import style from '../../articles.module.css';
 
 class VideoArticle extends Component {
+
+    state = {
+        article:[],
+        team:[]
+    }
+
+
+    UNSAFE_componentWillMount () {
+        axios.get(`${url}/videos/${this.props.params.id}`)
+        .then( response => {
+            let article = response.data;
+
+            axios.get(`${url}/teams/${article.team}`)
+            .then( response => {
+                this.setState({
+                    article,
+                    team: response.data
+                })
+            })
+        })
+    }
+
     render () {
+        console.log(this.state)
         return (
             <div>
                 Video Article Component
@@ -10,4 +38,4 @@ class VideoArticle extends Component {
     }
 }
 
-export default VideoArticle;
+export default withRouterHOC(VideoArticle);
