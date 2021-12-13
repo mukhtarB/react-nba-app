3bfd0b94205a4a804e7f837d158c94016cf0d129
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-12-13 09:15:04 +0100

    firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcgFmd0AwRF5hCJthkIobWBWRR0DdoPJc",
+  authDomain: "nba-react-app-46401.firebaseapp.com",
+  databaseURL: "https://nba-react-app-46401-default-rtdb.firebaseio.com",
+  projectId: "nba-react-app-46401",
+  storageBucket: "nba-react-app-46401.appspot.com",
+  messagingSenderId: "128238967683",
+  appId: "1:128238967683:web:f47b804b462a10622474b6",
+  measurementId: "G-Y3S76FDYXP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const firebaseDB = firebase.database();
+const dbArticles = firebaseDB.ref('articles');
+const dbTeams = firebaseDB.ref('teams');
+const dbVideos = firebaseDB.ref('videos');
+
+export {
+    firebase,
+    firebaseDB,
+    dbArticles,
+    dbVideos,
+    dbTeams
+}
