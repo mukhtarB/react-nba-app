45fda8e9313ee444dce0481556af6a021dcaa123
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-12-15 11:03:15 +0100

    component will mount fix

--- src/components/Articles/Videos/Video/video_index.js
+++ src/components/Articles/Videos/Video/video_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
-import { url } from '../../../../config';
+// import axios from "axios";
+// import { url } from '../../../../config';
+import { firebaseLooper, dbTeams, firebaseDB, dbVideos } from "../../../../firebase";
 import withRouterHOC from "../../../../hoc/withRouter/withRouter";
 
 import style from '../../articles.module.css';
@@ -18,39 +19,54 @@ class VideoArticle extends Component {
 
 
     UNSAFE_componentWillMount () {
+        firebaseDB.ref(`videos/${this.props.params.id}`).once('value')
+        .then( snapshot => {
+            let article = snapshot.val();
 
-        axios.get(`${url}/videos/${this.props.params.id}`)
-        .then( response => {
-            let article = response.data;
-
-            axios.get(`${url}/teams/${article.team}`)
-            .then( response => {
+            dbTeams.orderByChild("teamId").equalTo(article.team).once('value')
+            .then( snapshot => {
+                
+                const team = firebaseLooper(snapshot);
                 this.setState({
                     article,
-                    team: response.data
+                    team
                 })
             })
-
-            this.getRelated();
         })
+
+        // axios.get(`${url}/videos/${this.props.params.id}`)
+        // .then( response => {
+        //     let article = response.data;
+
+        //     axios.get(`${url}/teams/${article.team}`)
+        //     .then( response => {
+        //         this.setState({
+        //             article,
+        //             team: response.data
+        //         })
+        //     })
+
+        //     this.getRelated();
+        // })
 
     }
 
     getRelated = () => {
+        
 
-        axios.get(`${url}/teams`)
-        .then ( response => {
-            let teams = response.data
+        // axios.get(`${url}/teams`)
+        // .then ( response => {
+        //     let teams = response.data
 
-            axios.get(`${url}/videos?q=${this.state.team.city}&_limit=3`)
-            .then( response => {
-                this.setState({
-                    teams,
-                    related: response.data
-                })
-            })
+        //     axios.get(`${url}/videos?q=${this.state.team.city}&_limit=3`)
+        //     .then( response => {
+        //         this.setState({
+        //             teams,
+        //             related: response.data
+        //         })
+        //     })
             
-        })
+        // })
 
     }
 
@@ -58,10 +74,11 @@ class VideoArticle extends Component {
 
         const article = this.state.article;
         const team = this.state.team;
+        // console.log(team)
 
         return (
             <div>
-                <VidHeader teamData={team} />
+                <VidHeader teamData={team[0]} />
 
                 <div className={style.videoWrapper}>
                     <h1>{article.title}</h1>
