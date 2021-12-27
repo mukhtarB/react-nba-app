eb234e1bcb3890e5caebfc05d67dc618be38e4ec
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-12-27 11:01:48 +0100

    set up controlled form state

--- src/components/signIn/signIn.js
+++ src/components/signIn/signIn.js
@@ -5,7 +5,44 @@ import style from './signIn.module.css';
 
 class SignIn extends Component {
 
-    state = {}
+    state = {
+        registerError: '',
+        loading: false,
+        formData: {
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    type: 'input',
+                    name: 'email_input',
+                    placeholder: 'Enter your email'
+                },
+                validation: {
+                    required: true,
+                    email_rule: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+            },
+            password: {
+                element: 'input',
+                value: '',
+                config: {
+                    type: 'password',
+                    name: 'password_input',
+                    placeholder: 'Enter your password'
+                },
+                validation: {
+                    required: true,
+                    email_rule: true
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+            }
+        }
+    }
 
     render () {
         return (
