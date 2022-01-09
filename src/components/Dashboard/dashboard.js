0dbcefc4bea287c453bcd5592fd215f93ba5f5ec
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-09 14:33:46 +0100

    rendering the input field

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,10 +1,65 @@
 import React, { Component } from "react";
 
+import style from './dashboard.module.css'
+import FormField from "../../widgets/FormFields/formFields";
+
 class Dashboard extends Component {
+
+    state = {
+        postError: '',
+        loading: false,
+        formData: {
+            author: {
+                element: 'input',
+                value: '',
+                config: {
+                    type: 'text',
+                    name: 'author_input',
+                    placeholder: 'Enter your name?'
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+            },
+            title: {
+                element: 'input',
+                value: '',
+                config: {
+                    type: 'text',
+                    name: 'title_input',
+                    placeholder: 'Enter your title?'
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: '',
+            },
+        }
+    }
+
+    updateFormWith = (newState) => {
+        
+    }
 
     render () {
         return(
-            <div>Dashboard Component</div>
+            <div className={style.postContainer}>
+                <form onSubmit={this.submitForm}>
+                    <h2>Add Post</h2>
+
+                    <FormField
+                        id={'author'}
+                        formFieldData = {this.state.formData.author}
+                        change = {(newState) => this.updateFormWith(newState)}
+                    />
+
+                </form>
+            </div>
         )
     }
 }
