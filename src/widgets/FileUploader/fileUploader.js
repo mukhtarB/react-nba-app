393381bc9660756c83e3b9c5f432704079347e2c
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-17 14:27:43 +0100

    setting up & rendering fileUploader

--- src/widgets/FileUploader/fileUploader.js
+++ src/widgets/FileUploader/fileUploader.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import { firebase } from '../../firebase';
+import FileUploader from 'react-firebase-file-uploader';
 
 class Uploader extends Component {
+    
+    state = {
+        fileName: '',
+        isUploading: false,
+        progress: 0,
+        fileURL: '',
+    }
 
     render () {
         return (
             <div>
-                File Uploader
+                <FileUploader
+                    accept='image/*'
+                    name="image"
+                    randomizeFilename
+                    storageRef={firebase.storage().ref('images')}
+                    onUploadStart={this.handleUploadStart}
+                    onUploadError={this.handleUploadError}
+                    onUploadSuccess={this.handleUploadSuccess}
+                    onProgress={this.handleProgress}
+                />
             </div>
         )
     }
