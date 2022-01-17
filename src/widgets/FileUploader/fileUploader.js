0d06dfcea2fa5b49739eeff549fe74a093c28bb3
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-17 14:15:50 +0100

    file uploader component setup

---
+++ src/widgets/FileUploader/fileUploader.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { firebase } from '../../firebase';
+
+class Uploader extends Component {
+
+    render () {
+        <div>
+            File Uploader
+        </div>
+    }
+}
+
+export default Uploader;
