95d690862cb06216c72e0e8fd805fd88ff171f5a
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-23 16:19:12 +0100

    update withRouter

--- src/hoc/withRouter/withRouter.js
+++ src/hoc/withRouter/withRouter.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { useParams, useNavigate } from "react-router";
+import { useParams, useNavigate, useLocation } from "react-router";
 
 const withRouterHOC = WrappedComponent => props => {
     const params = useParams();
     const navigate = useNavigate();
+    const location = useLocation();
     // etc... other react-router-dom v6 hooks
     
     return (
-        <WrappedComponent {...props} params={params} navigate={navigate} />
+        <WrappedComponent
+            {...props}
+            params={params}
+            navigate={navigate}
+            location={location}
+        />
     );
 };
 
