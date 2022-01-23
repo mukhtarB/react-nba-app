ad33b1a99ca6795ad79a8dde0c268df12ff6c6ce
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-01-23 16:23:55 +0100

    alternate private route logic

--- src/components/AuthRoutes/privateRoutes.js
+++ src/components/AuthRoutes/privateRoutes.js
@@ -1,22 +1,34 @@
 import React, { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const PrivateRoute = ({
     user,
     children
 }) => {
 
-    // METHOD 1
-    const navigate = useNavigate();
     const location = useLocation();
-    useEffect( ()=> {
-        if (!user) {
-            navigate('/sign-in', {state: {from: location} });
-        }
-    })
+
+    // METHOD 1
+    // const navigate = useNavigate();
+    
+    // useEffect( ()=> {
+    //     if (!user) {
+    //         navigate('/sign-in', {state: {from: location} });
+    //     }
+    // })
+
+    // return (
+    //     <React.Fragment>
+    //         {children}
+    //     </React.Fragment>
+    // )
+    
+
+    // METHOD 2 - comes with redirect to prev page for UX boost
+
     return (
         <React.Fragment>
-            {children}
+            { user ? children : <Navigate to='/sign-in' replace state={{ from:location }} />}
         </React.Fragment>
     )
 }
